fca5427332d7479e81d9f890739d2c4075ba082c
Matheus C. Manoel <matheuscmanoel@gmail.com>
2016-01-15 19:48:16 -0200

    Add users index page with pagination

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,6 +6,11 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:exemplo)
     @other_user = users(:exemplo2)
   end
+
+  test "should redirect index when not logged in" do
+    get :index, id: @user
+    assert_redirected_to login_url
+  end
 	
   test "should get new" do
     get :new
