0fae25d86a57b19cbe11032c082379f2e433dc14
Matheus C. Manoel <matheuscmanoel@gmail.com>
2016-01-21 23:47:26 -0200

    Add delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy 
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_path
+  end
+
   private
 
     def user_params
@@ -65,4 +72,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+    # Confirms an admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
