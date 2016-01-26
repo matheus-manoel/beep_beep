e98029d54ceb98df90ad801f32425c5c2a2da592
Matheus C. Manoel <matheuscmanoel@gmail.com>
2016-01-26 20:25:37 -0200

    Add update and edit actions and views for password_resets controller.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Sample twitter clone built with ruby on rails.
 
 #todo
 - Can't access sign up page if logged in.
+- Test password_reset_expired?
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,6 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+
   def new
   end
 
@@ -16,6 +18,37 @@ class PasswordResetsController < ApplicationController
     end
 
   def edit
-    
   end
+
+  def update
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    elsif (params[:user][:password].blank? ||
+        params[:user][:password_confirmation].blank?)
+      flash.now[:danger] = "Password/confirmation can't be blank."
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in(@user)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+      unless (@user && !@user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
 end
