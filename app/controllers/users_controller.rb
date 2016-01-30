d9c0ccc2515a353f61cf6b18bfd51445072abf97
matheus-manoel <matheuscmanoel@gmail.com>
2016-01-30 16:52:06 -0200

    Modification to test heroku app.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
+      #@user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url #it could be 'redirect_to @user' as well.
     else
