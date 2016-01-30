55d621fbea68faea824291f67ab0c3acb19ad546
matheus-manoel <matheuscmanoel@gmail.com>
2016-01-30 17:03:28 -0200

    Fix migration spelling error.

--- db/migrate/20160123053855_add_activations_to_users.rb
+++ db/migrate/20160123053855_add_activations_to_users.rb
@@ -1,7 +1,7 @@
 class AddActivationsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :activation_digest, :string
-    add_column :users, :avtivated, :boolean, default: false
+    add_column :users, :activated, :boolean, default: false
     add_column :users, :activated_at, :datetime
   end
 end
--- db/migrate/20160123061418_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration
-  def change
-   rename_column :users, :avtivated, :activated
-  end
-end
