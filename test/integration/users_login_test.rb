77ceb4a7905975cafe4384d01945233249be0622
matheus-manoel <matheuscmanoel@gmail.com>
2016-02-01 15:50:51 -0200

    Add microposts to user profile. Add user profile integration test.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -25,7 +25,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", users_path, count: 1
-    assert_select "a[href=?]", user_path(@user), count: 1
     assert_select "a[href=?]", logout_path, count: 1
     delete logout_path
     assert_not is_logged_in?
