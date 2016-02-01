945c662a69dcb100d931c7cf5104700ca0da7018
matheus-manoel <matheuscmanoel@gmail.com>
2016-01-31 22:21:27 -0200

    Add order desc to microposts and deletion of microposts associated with deleted user. Add tests as well.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:exemplo)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -24,6 +24,10 @@ class MicropostTest < ActiveSupport::TestCase
   test "content should be at most 140 characters" do
     @micropost.content = "a" * 141
     assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal Micropost.first, microposts(:most_recent)
   end
 
 end
