1e3b16f7b89b6d2ca33f22bd06f8f752362e2902
matheus-manoel <matheuscmanoel@gmail.com>
2016-02-02 22:08:28 -0200

    Begin implementation of create and destroy actions in the microposts controller. Add respective tests.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,12 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+ 
+  def create
+    
+  end
+
+  def destroy
+    
+  end
+   
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+    @micropost = microposts(:orange)
+	end
+  
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: "Lorem ipsum" }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: @micropost
+    end
+    assert_redirected_to login_url
+  end
+
 end
